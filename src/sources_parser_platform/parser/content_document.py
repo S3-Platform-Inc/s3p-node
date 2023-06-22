989b6e0f72fa038a78246a7af0bbfe81f1fd60ca
CuberHuber <lupashcko.rom@yandex.ru>
2023-06-22 16:09:07 +0500

    #13 Модуль фильтрации.

--- src/sources_parser_platform/parser/content_document.py
+++ src/sources_parser_platform/parser/content_document.py
@@ -1,27 +1,34 @@
+"""
+Тип данных для парсера и этапов обработки
+"""
+
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class Content_Document:
-    title: str          # Заголовок
-    pub_date: str       # Дата публикации
-    abstract: str       # Пока пустое поле
-    web_link: str       # Ссылка на веб-страницу, где можно получить (или прочитать) содержимое Документа
-    local_link: str     # Локальная ссылка на загруженный файл с содержимым документа
-    load_date: str      # Дата загрузки документа
-    add_data: str       # Пока пустое поле
-    category: str       # Категория материала
-    sub_category: str   # Подкатегория материала
-    version: str        # Версия документа
-    source_name: str    #
+    """
+    Унифицированный тип данных, который используется при
+    """
 
+    title: str  # Заголовок
+    pub_date: datetime  # Дата публикации
+    abstract: str  # Пока пустое поле
+    web_link: str  # Ссылка на веб-страницу, где можно получить (или прочитать) содержимое Документа
+    local_link: str  # Локальная ссылка на загруженный файл с содержимым документа
+    load_date: datetime  # Дата загрузки документа
+    add_data: str  # Пока пустое поле
+    category: str  # Категория материала
+    sub_category: str  # Подкатегория материала
+    version: str  # Версия документа
+    source_name: str  # Уникальное название источника
 
-class Document:
+    def __hash__(self):
+        """
+        Для проверки уникальности и новизны документа.
 
-    def __init__(self, metadata: Content_Document):
-
-        self.metadata = metadata
-        ...
-
-    def download(self):
-        ...
+        :return:
+        :rtype:
+        """
+        return hash((self.title, self.web_link, self.pub_date))
