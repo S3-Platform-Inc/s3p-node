16bcfd3b008f51e5840344291fc96de3378b0c70
CuberHuber <lupashcko.rom@yandex.ru>
2024-03-20 17:11:28 +0300

    init script for makes dirs

---
+++ init.sh
@@ -0,0 +1,26 @@
+
+
+# Create dirs for localstorages
+mkdir ./"$LS_BASE_TEMP_DIR"/
+mkdir -p "$LS_BASE_TEMP_DIR"/n1/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n2/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n3/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n4/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n5/"$LS_WORK_DIR"
+
+
+# Create dirs for logs
+mkdir ./"$SPP_LOG_TEMP_PATH"/
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n1
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n2
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n3
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n4
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n5
+
+# Create dirs for plugins archive
+mkdir ./"$PL_BASE_TEMP_DIR"/
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n1
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n2
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n3
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n4
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n5
