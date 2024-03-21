d6e6ba3da259c1d314006fddb8df5c936d9ece02
CuberHuber <lupashcko.rom@yandex.ru>
2024-03-21 18:57:13 +0300

    10 nodes

--- init.sh
+++ init.sh
@@ -7,6 +7,11 @@ mkdir -p "$LS_BASE_TEMP_DIR"/n2/"$LS_WORK_DIR"
 mkdir -p "$LS_BASE_TEMP_DIR"/n3/"$LS_WORK_DIR"
 mkdir -p "$LS_BASE_TEMP_DIR"/n4/"$LS_WORK_DIR"
 mkdir -p "$LS_BASE_TEMP_DIR"/n5/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n6/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n7/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n8/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n9/"$LS_WORK_DIR"
+mkdir -p "$LS_BASE_TEMP_DIR"/n10/"$LS_WORK_DIR"
 
 
 # Create dirs for logs
@@ -16,6 +21,11 @@ mkdir -p ./"$SPP_LOG_TEMP_PATH"/n2
 mkdir -p ./"$SPP_LOG_TEMP_PATH"/n3
 mkdir -p ./"$SPP_LOG_TEMP_PATH"/n4
 mkdir -p ./"$SPP_LOG_TEMP_PATH"/n5
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n6
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n7
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n8
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n9
+mkdir -p ./"$SPP_LOG_TEMP_PATH"/n10
 
 # Create dirs for plugins archive
 mkdir ./"$PL_BASE_TEMP_DIR"/
@@ -24,3 +34,8 @@ mkdir -p ./"$PL_BASE_TEMP_DIR"/n2
 mkdir -p ./"$PL_BASE_TEMP_DIR"/n3
 mkdir -p ./"$PL_BASE_TEMP_DIR"/n4
 mkdir -p ./"$PL_BASE_TEMP_DIR"/n5
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n6
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n7
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n8
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n9
+mkdir -p ./"$PL_BASE_TEMP_DIR"/n10
